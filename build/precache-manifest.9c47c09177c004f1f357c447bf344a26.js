660bf77ecd100541ef1c7347d0d4c8dff9ef819e
vitaly.stores <vitaly.stores@gmail.com>
2020-07-18 03:59:49 +0300

    initial

---
+++ build/precache-manifest.9c47c09177c004f1f357c447bf344a26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3030ca4aa424cf6ff5ab3bc0e45955",
+    "url": "/evraz-app/index.html"
+  },
+  {
+    "revision": "23191db1276b1a6de63f",
+    "url": "/evraz-app/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "4051b1e6265d85de3fc9",
+    "url": "/evraz-app/static/css/main.3ba7d72d.chunk.css"
+  },
+  {
+    "revision": "23191db1276b1a6de63f",
+    "url": "/evraz-app/static/js/2.df1882e1.chunk.js"
+  },
+  {
+    "revision": "96444f998001cf9002746183228bddf3",
+    "url": "/evraz-app/static/js/2.df1882e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4051b1e6265d85de3fc9",
+    "url": "/evraz-app/static/js/main.79c4ff05.chunk.js"
+  },
+  {
+    "revision": "b28f4e3b39dd6d057592",
+    "url": "/evraz-app/static/js/runtime-main.0afd5622.js"
+  },
+  {
+    "revision": "4555ff1db885251a4e81ba12ae1b1883",
+    "url": "/evraz-app/static/media/logo--en.4555ff1d.svg"
+  }
+]);
